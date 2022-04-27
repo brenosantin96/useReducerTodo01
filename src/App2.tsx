0047ab16eeada4110311413227942f53740ccfda
Breno Santin <brenolimaa96@gmail.com>
2022-04-27 16:39:00 +0200

    segundo commit

--- src/App2.tsx
+++ src/App2.tsx
@@ -6,31 +6,49 @@ type todoType = {
     todoDone: boolean
 }
 
-const initialTodos : todoType[] = {
-    
+type actionType = {
+    type: string
 }
 
-export const App2 = () => {
+const initialTodos = {
+    todos: [] as todoType[],
+};
 
-    const [inputInfo, setInputInfo] = useState('');
 
-    const [todo, dispatch] = useReducer(reducer, <initialTodos[]>) 
-
-    const handleInputTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputInfo(e.currentTarget.value);
+const reducer = (state: todoType, action: actionType) => {
+    switch (action.type) {
+        case 'add-todo':
+            return { ...state, count: state.count + 1 };
+        case 'delete-todo':
+            return { ...state, count: state.count - 1 };
     }
 
-    const handleSubmit = (e: React.FormEvent)=> {
-        e.preventDefault();
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit} >
-                <input type="text" onChange={handleInputTodo} />
-            </form>
-            <Todo></Todo>
-
-        </div>
-    )
+    return state;
 }
+
+
+
+    export const App2 = () => {
+
+        const [inputInfo, setInputInfo] = useState('');
+
+        const [todo, dispatch] = useReducer(reducer, initialTodos)
+
+        const handleInputTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+            setInputInfo(e.currentTarget.value);
+        }
+
+        const handleSubmit = (e: React.FormEvent) => {
+            e.preventDefault();
+        }
+
+        return (
+            <div>
+                <form onSubmit={handleSubmit} >
+                    <input type="text" onChange={handleInputTodo} />
+                </form>
+                <Todo></Todo>
+
+            </div>
+        )
+    }
